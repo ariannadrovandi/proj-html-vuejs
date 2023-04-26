78967c782a633300f460f351abeb4b8be0a79d41
ariannadrovandi <ariannadrovandi02@gmail.com>
2023-04-26 19:32:43 +0200

    fix HeaderTop

--- src/components/HeaderBottom.vue
+++ src/components/HeaderBottom.vue
@@ -1,5 +1,22 @@
 <template>
-  
+    <!-- <div class="container d-flex flex-column justify-content-around align-items-center">
+        <div class="image">
+            <img src="../../public/img/avada-food-logo.png" alt="logo">
+        </div>
+        <div class="navbar">
+            <nav>
+                <ul class="d-flex flex-row">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Recipes</a></li>
+                    <li><a href="#">Places</a></li>
+                    <li><a href="#">Bolg</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><i class="fa-solid fa-magnifying-glass"></i></li>
+                </ul>
+            </nav>
+        </div>
+    </div> -->
 </template>
 
 <script>
@@ -14,5 +31,11 @@
 </script>
 
 <style lang="scss" scoped>
-  
+    // ul{
+    //     list-style: none;
+    //     li{
+    //         margin-left: 35px;
+    //         margin-right: 35px;
+    //     }
+    // }
 </style>
--- src/components/HeaderTop.vue
+++ src/components/HeaderTop.vue
@@ -1,10 +1,8 @@
 <template>
     <div class="container d-flex flex-row justify-content-between align-items-center">
-        <div class="left">
-            <ul class="d-flex flex-row">
-                <li><button class="btn">Download App <span class="badge ad-badge">iOS</span></button></li>
-                <li><button class="btn">Advertise with us</button></li>
-            </ul>
+        <div class="left d-flex flex-row">
+            <button class="btn">Download App <span class="badge ad-badge">iOS</span></button>
+            <button class="btn">Advertise with us</button>
         </div>
         <div class="right">
             <ul class="d-flex flex-row ">
@@ -27,12 +25,12 @@
 <style lang="scss" scoped>
     ul{
         list-style: none;
-        .ad-badge{
-            background-color: orange;
-            color: white;
-        };
-        i{
-            color: gray;
-        }
+    }
+    .ad-badge{
+        background-color: orange;
+        color: white;
+    }
+    i{
+        color: gray;
     }
 </style>
