748ef12bedda4524ad1f28bc23a9b5b73616a86d
ariannadrovandi <ariannadrovandi02@gmail.com>
2023-04-28 17:04:33 +0200

    footer bottom

--- src/components/FooterBottom.vue
+++ src/components/FooterBottom.vue
@@ -1,5 +1,7 @@
 <template>
-    
+    <div class="container">
+        <p>© Copyright 2012 - 2020 | Avada Theme by ThemeFusion I AIl Rights Reserved I Powered by WordPress</p>
+    </div>
 </template>
 
 <script>
@@ -10,5 +12,14 @@
 
 <style lang="scss" scoped>
     @use '../assets/styles/partials/variables' as *;
-    
+    .container{
+        padding: 10px;
+    }
+    p{
+        margin-bottom: 20px;
+        padding: 0;
+        text-align: center;
+        font-size: 13px;
+        color: $dark-gray;
+    }
 </style>
